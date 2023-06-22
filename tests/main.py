b6d3d5f50cc84778bf810d1c57f66a45f83071b0
Çağan Mert İŞLEK <islekcaganmert@gmail.com>
2023-06-22 17:24:16 +0300

    Env for pypi

---
+++ tests/main.py
@@ -0,0 +1,16 @@
+from bevyframe import Frame, renderpage, request, redirect
+
+class routes:
+    def index():
+        usernames = {'1234567890': 'demo'}
+        username = usernames[request.cookies['session']]
+        return renderpage('index.html', cuser=username)
+    def login():
+        request.cookies['session'] = '1234567890'
+        return redirect('/')
+    def __error_page__(error):
+        return '<h1>Error Code: '+str(error)+'</h1>'
+
+if __name__ == '__main__':
+    paths = {'/': [routes.index, {}], '/login': [routes.login, {}]}
+    Frame(routes, paths).start_server(host='0.0.0.0', port=8000)
