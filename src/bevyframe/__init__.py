15532d48a2f21d3a58e461ef006da1401e197aef
islekcaganmert <islekcaganmert@hereus.net>
2024-02-21 06:39:40 +0300

    Static File Serving Fix

--- src/bevyframe/__init__.py
+++ src/bevyframe/__init__.py
@@ -344,8 +344,11 @@ class Frame:
                 r = f"{recv['protocol']} {resp.status_code} {https_codes[resp.status_code]}\r\n"
                 for header in resp.headers:
                     r += f"{header}: {resp.headers[header]}\r\n"
-                r += f"\r\n{resp.body}"
+                r += f"\r\n"
                 r = r.encode()
+                if not isinstance(resp.body, bytes):
+                    resp.body = resp.body.encode()
+                r += resp.body
                 client_socket.sendall(r)
                 client_socket.close()
                 print(f'\r({resp.status_code})')
